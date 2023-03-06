ef0afb30c9accd239dcb2a21b58e13f90e202962
Sonam-Chand <111940478+Sonam-Chand@users.noreply.github.com>
2023-03-06 15:30:44 +1100

    snapshot added

---
+++ snapshots/sms_record.sql
@@ -0,0 +1,18 @@
+{% snapshot sms_record_snapshot %}
+
+
+{{
+    config(
+      target_database='sana-sandbox-356306',
+      target_schema='my_first_dbt_model',
+      unique_key='subscription_id',
+      strategy='timestamp',
+      updated_at='start_of_charge_dt',
+    )
+}}
+
+select * from {{ source('sana-sandbox-356306', 'sms_record') }}
+
+
+
+{% endsnapshot %}
