6c9fc6b95f4615f1e6d6086f530b16c4437def10
Sonam-Chand <111940478+Sonam-Chand@users.noreply.github.com>
2023-03-09 10:52:29 +1100

    snapshot indentation fixed

--- snapshots/unique_small_sms_record.sql
+++ snapshots/unique_small_sms_record.sql
@@ -1,22 +1,15 @@
+{% snapshot unique_sms_record_snapshot %}
 
-{% snapshot unique_sms_record_snapshot %} 
-
-
-{{
-    config
-(
-    target_database='sana-sandbox-356306',
+    {{
+        config(
+          target_database='sana-sandbox-356306',
     target_schema='my_first_dbt_model',
     unique_key='subscription_id',
     strategy='timestamp',
     updated_at='start_of_charge_dt',
-    )
-}}
+        )
+    }}
 
-select *
-from {{ source
-('sana-sandbox-356306', 'unique_sms_record') }}
-
-
+    select * from {{ source('sana-sandbox-356306', 'unique_sms_record') }}
 
 {% endsnapshot %}
