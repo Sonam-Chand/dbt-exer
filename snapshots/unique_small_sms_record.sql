668059e7806e6abf168a084d5eee86273721dda5
Sonam-Chand <111940478+Sonam-Chand@users.noreply.github.com>
2023-03-09 10:47:10 +1100

    snapshot indentations all

--- snapshots/unique_small_sms_record.sql
+++ snapshots/unique_small_sms_record.sql
@@ -1,15 +1,14 @@
-
 { % snapshot unique_sms_record_snapshot % } 
 
 
 {{
     config
 (
-      target_database='sana-sandbox-356306',
-      target_schema='my_first_dbt_model',
-      unique_key='subscription_id',
-      strategy='timestamp',
-      updated_at='start_of_charge_dt',
+    target_database='sana-sandbox-356306',
+    target_schema='my_first_dbt_model',
+    unique_key='subscription_id',
+    strategy='timestamp',
+    updated_at='start_of_charge_dt',
     )
 }}
 
