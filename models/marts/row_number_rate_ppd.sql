d4c3327f7def690c52edfe2778585951936cab55
Sonam-Chand <111940478+Sonam-Chand@users.noreply.github.com>
2023-03-09 12:58:39 +1100

    row  number sql model1

--- models/marts/row_number_rate_ppd.sql
+++ models/marts/row_number_rate_ppd.sql
@@ -6,9 +6,9 @@ select
         start_date,
         end_date,
         active_flag,
-        row_number() over (partition by rate_plan_name order by start_date) as row_n
+        row_number() over (partition by rate_plan_name ) as row_n
 from {{ ref('stg_rate_plan_product') }}
-order by 1
+order by 2
 )
 select * from ref
 
