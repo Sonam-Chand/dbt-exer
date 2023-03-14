52407b1ea7f0c0cb00a03ad0e5c62cd89e6108a6
Sonam-Chand <111940478+Sonam-Chand@users.noreply.github.com>
2023-03-14 11:17:11 +1100

    dbt util package added

--- models/marts/row_number_rate_ppd.sql
+++ models/marts/row_number_rate_ppd.sql
@@ -1,17 +1,22 @@
-with ref as(
-select
-        rate_plan_product_id,
-        rate_plan_name,
-        rate_plan_description,
-        start_date,
-        end_date,
-        active_flag,
-        row_number() over (partition by rate_plan_name order by rate_plan_product_id) as row_n
-
-from {{ ref('stg_rate_plan_product') }}
-order by 1
-
+WITH ref AS(
+        SELECT
+                rate_plan_product_id,
+                rate_plan_name,
+                rate_plan_description,
+                start_date,
+                end_date,
+                active_flag,
+                ROW_NUMBER() over (
+                        PARTITION BY rate_plan_name
+                        ORDER BY
+                                rate_plan_product_id
+                ) AS row_n
+        FROM
+                {{ ref('stg_rate_plan_product') }}
+        ORDER BY
+                1
 )
-select * from ref
-
-             
+SELECT
+        *
+FROM
+        ref
