ea3c8f1fa9760f27cd7674d79091be65512e6809
Sonam-Chand <111940478+Sonam-Chand@users.noreply.github.com>
2023-03-15 17:30:23 +1100

    tested incremental and macro

--- models/marts/incremental.sql
+++ models/marts/incremental.sql
@@ -1,23 +1,22 @@
---depends_on: {{ref('stg_unique_sms_record')}}
-{
-{
-    config
-(
-        materialized='incremental'
-    )
-}}
+{{ config (
+    materialized = 'incremental'
+) }}
 
-with base as(
-select *
-from {{ref
-('stg_unique_sms_record')}}
-    {%if is_incremental() %}
-    where start_of_charge_dt >= max
-(start_of_charge_dt) from {{this}}
-    {% endif %})
+WITH base AS(
 
+    SELECT
+        *
+    FROM
+        {{ ref('stg_unique_sms_record') }}
 
-select * from base;
-
-
-
+{% if is_incremental() %}
+WHERE
+    start_of_charge_dt >= MAX(start_of_charge_dt)
+FROM
+    {{ this }}
+{% endif %}
+)
+SELECT
+    *
+FROM
+    base
